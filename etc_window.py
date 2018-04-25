eb7124e9c9b6603ef5bdf4626175b13f3a6f74dc
Álvaro Callejas <amcallejasherrera@gmail.com>
2018-04-24 20:16:27 -0400

    Organize window in two panes

--- etc_window.py
+++ etc_window.py
@@ -1,3 +1,5 @@
+import etc_window_lpane as lpane
+import etc_window_rpane as rpane
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
@@ -13,9 +15,31 @@ class ETC_Window(Gtk.ApplicationWindow):
         Gtk.Window.__init__(self)
         self.set_title(win_title + " " + win_subtitle)
         self.set_application(app_name)
-
         headerbar = Gtk.HeaderBar()
         headerbar.props.show_close_button = True
         headerbar.props.title = win_title
         headerbar.props.subtitle = win_subtitle
         self.set_titlebar(headerbar)
+
+        # A Gtk.Grid as main container
+        grid = Gtk.Grid()
+        # Adding grid to window
+        self.add(grid)
+
+        # A Gtk.InfoBar as top statusbar
+        self.infobar = Gtk.InfoBar()
+        self.infobar.set_message_type(Gtk.MessageType.INFO)
+        infobar_label = Gtk.Label()
+        infobar_content = self.infobar.get_content_area()
+        infobar_content.add(infobar_label)
+        # Attaching infobar to grid
+        grid.attach(self.infobar, 0, 0, 1, 1)
+
+        # A Gtk.Paned horizontally as secondary container
+        hpaned = Gtk.Paned()
+        hpaned.set_wide_handle(True)
+        # Attaching hpaned to grid
+        grid.attach(hpaned, 0, 1, 1, 1)
+        # Adding panes to hpaned
+        hpaned.add1(lpane.ETC_Window_lpane())
+        hpaned.add2(rpane.ETC_Window_rpane())
---
+++ etc_window_lpane.py
@@ -0,0 +1,8 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+
+class ETC_Window_lpane(Gtk.Grid):
+    def __init__(self):
+        Gtk.Grid.__init__(self)
