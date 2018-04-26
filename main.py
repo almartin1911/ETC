558ae84c04e91ecd713a72636a0c50788d05bfe7
Álvaro Callejas <amcallejasherrera@gmail.com>
2018-04-26 16:52:53 -0400

    Refactor the import

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
-import etc_application as application
+import etc_application
 
 
 if __name__ == '__main__':
-    etc = application.ETC_Application()
+    etc = etc_application.ETC_Application()
     exit_status = etc.run(sys.argv)
     sys.exit(exit_status)
