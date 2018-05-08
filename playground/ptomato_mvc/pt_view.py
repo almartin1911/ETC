03f0961c8e4c36510cec3a88dfe6b45071b76201
Alvaro Callejas <amcallejasherrera@gmail.com>
2018-05-08 09:26:03 -0400

    Add a Gtk.Grid and a Gtk.StackSwitcher

--- playground/ptomato_mvc/pt_view.py
+++ playground/ptomato_mvc/pt_view.py
@@ -12,21 +12,31 @@ class View(Gtk.ApplicationWindow):
     }
 
     def __init__(self, **kw):
-        super(View, self).__init__(default_width=400, default_height=300, **kw)
+        super(View, self).__init__(**kw)
+
+        self._grid = Gtk.Grid(vexpand=True)
+        self.add(self._grid)
 
         self._stack = Gtk.Stack(
             transition_duration=500,
             transition_type=Gtk.StackTransitionType.CROSSFADE
         )
-        self._button = Gtk.Button(label='Click me',
-                                  halign=Gtk.Align.CENTER,
-                                  valign=Gtk.Align.CENTER)
-        self._label = Gtk.Label()
-        self._stack.add_named(self._button, self.BUTTON_PAGE)
-        self._stack.add_named(self._label, self.GREETING_PAGE)
-        self.add(self._stack)
 
+        self._button = Gtk.Button(label='Click me')
         self._button.connect('clicked', self._on_clicked)
+        self._stack.add_titled(self._button, self.BUTTON_PAGE,
+                               self.BUTTON_PAGE)
+
+        self._label = Gtk.Label()
+        self._stack.add_titled(self._label, self.GREETING_PAGE,
+                               self.GREETING_PAGE)
+        # self.add(self._stack)
+
+        self._stack_switcher = Gtk.StackSwitcher()
+        self._stack_switcher.set_stack(self._stack)
+
+        self._grid.attach(self._stack_switcher, 0, 0, 1, 1)
+        self._grid.attach(self._stack, 0, 1, 1, 1)
 
     def _on_clicked(self, button, *args):
         self.emit('button-clicked')
